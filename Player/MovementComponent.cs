85199e4da5847bf440ca3fc159d76e6cf9a19cda
Joachim <joachim@kanpeki.com>
2024-03-24 00:33:12 +0100

    Adrenaline boost when Seeker sees a prey
    https://www.pivotaltracker.com/story/show/187298912

--- Player/MovementComponent.cs
+++ Player/MovementComponent.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using static PlayerInfo;
 
 public partial class MovementComponent : Node
 {
@@ -8,14 +9,24 @@ public partial class MovementComponent : Node
 	[Export]
 	public float MovementSpeed = 200f;
 	[Export]
-	public float SprintSpeedMultiplier = 3f;
+	public float MovementSpeedMultiplier = 1f;
+	[Export]
+	public float CrouchSpeedMultiplier = 0.25f;
+	[Export]
+	public float SprintSpeedMultiplier = 2f;
+	[Export]
+	public float SpeedBoostMultiplier = 1.15f;
+	[Export]
+	public float SpeedBoostDuration = 1f;
 
 
 	private PlayerInput input { get; set; }
 	private AnimatedSprite2D animatedSprite2D;
-	private float baseMovementSpeed;
 	private GpuParticles2D dustTrail;
-	private Player localPlayer
+	private float movementSpeedMultiplier;
+	private float calculatedMovementSpeed { get; set; }
+
+	private Player playerNode
 	{
 		get => GetParent<Player>();
 	}
@@ -23,38 +34,39 @@ public partial class MovementComponent : Node
 
 	public override void _Ready()
 	{
-		animatedSprite2D = localPlayer.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D = playerNode.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		dustTrail = GetNode<GpuParticles2D>("Effects/DustTrail");
 		input = GetNode<PlayerInput>("PlayerInput");
-		baseMovementSpeed = MovementSpeed;
 	}
 
 	public override void _Process(double delta)
 	{
-		if(localPlayer.IsRunning && !localPlayer.Velocity.IsZeroApprox() && localPlayer.CurrentStamina>0)
-		{
-			if (!dustTrail.Emitting)
-			{
-				dustTrail.Restart();
-			}
-		}
-		else
-		{
-			dustTrail.Emitting = false;
-		}
+		//if(playerNode.IsRunning && !playerNode.Velocity.IsZeroApprox() && playerNode.CurrentStamina>0)
+		//{
+		//	if (!dustTrail.Emitting)
+		//	{
+		//		dustTrail.Restart();
+		//	}
+		//}
+		//else
+		//{
+		//	dustTrail.Emitting = false;
+		//}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		UpdateCrouchingState(delta);
 		UpdateSprintingState(delta);
+		UpdateChaseModeState(delta);
+		SetPlayerSpeed();
 		UpdatePlayerPosition(delta);
 	}
 
 	private void UpdateCrouchingState(double delta)
 	{
-		localPlayer.IsCrouching = input.Crouching;
-		if (localPlayer.IsCrouching)
+		playerNode.IsCrouching = input.Crouching;
+		if (playerNode.IsCrouching)
 		{
 			animatedSprite2D.Animation = Globals.PlayerAnimation.Crouch.ToString();
 		}
@@ -64,33 +76,89 @@ public partial class MovementComponent : Node
 		}
 	}
 
+
 	private void UpdateSprintingState(double delta)
 	{
-		localPlayer.IsRunning = input.Running;
-		if (localPlayer.IsRunning && localPlayer.CurrentStamina>0)
-		{
-			MovementSpeed = baseMovementSpeed * SprintSpeedMultiplier;
 
-			//animatedSprite2D.Animation = Globals.PlayerAnimation.Crouch.ToString();
+		if (playerNode.CurrentStamina < 1  || ( Math.Min(Math.Abs(playerNode.Velocity.X),1) < 1 && Math.Min(Math.Abs(playerNode.Velocity.Y),1) < 1))
+		{
+			playerNode.IsRunning = false;
+		}
+		else { 
+			playerNode.IsRunning = input.Running;
+		}
+		if (playerNode.IsRunning)
+		{
+			//Set animation for running
+			if (!dustTrail.Emitting)
+			{
+				dustTrail.Restart();
+			}
 		}
 		else
 		{
-			MovementSpeed = baseMovementSpeed * 1;
-
-			//animatedSprite2D.Animation = Globals.PlayerAnimation.Walk.ToString();
+			//Unset animation for running
+			dustTrail.Emitting = false;
 		}
+	}
+
+	private void UpdateChaseModeState(double delta)
+	{
+		if(playerNode.Role != PlayerInfo.PlayerRole.Seeker)
+		{
+			return;
+		}
+		if(playerNode.IsInChaseMode)
+		{
+			SpeedBoost();
+		}
+	}
+
+	private void SetPlayerSpeed()
+	{
+		movementSpeedMultiplier = MovementSpeedMultiplier;
+		if (playerNode.IsRunning)
+		{
+			movementSpeedMultiplier *= SprintSpeedMultiplier;
+		}
+		if (playerNode.IsCrouching)
+		{
+			movementSpeedMultiplier *= CrouchSpeedMultiplier;
+		}
+		if (playerNode.IsBoosted)
+		{
+			movementSpeedMultiplier *= SpeedBoostMultiplier;
+		}
+		calculatedMovementSpeed = MovementSpeed * movementSpeedMultiplier;
 	}
 
 	private void UpdatePlayerPosition(double delta)
 	{
 		Vector2 direction = input.Direction.Normalized();
-		localPlayer.Velocity = localPlayer.Velocity.Lerp(direction * MovementSpeed, 0.1f);
-		localPlayer.Velocity = localPlayer.Velocity * (float)(1 - (Friction * delta));
-		localPlayer.MoveAndSlide();
-		localPlayer.Position = new Vector2(
-			x: Mathf.Clamp(localPlayer.Position.X, 0, Level.ScreenSize.X),
-			y: Mathf.Clamp(localPlayer.Position.Y, 0, Level.ScreenSize.Y)
+		playerNode.Velocity = playerNode.Velocity.Lerp(direction * calculatedMovementSpeed, 0.1f);
+		playerNode.Velocity = playerNode.Velocity * (float)(1 - (Friction * delta));
+		playerNode.MoveAndSlide();
+		playerNode.Position = new Vector2(
+			x: Mathf.Clamp(playerNode.Position.X, 0, Level.ScreenSize.X),
+			y: Mathf.Clamp(playerNode.Position.Y, 0, Level.ScreenSize.Y)
 		);
+	}
+
+	public void SpeedBoost()
+	{
+		Timer speedBoostTimer = GetNode<Timer>("SpeedBoostTimer");
+		playerNode.IsBoosted = true;
+		if (!speedBoostTimer.IsStopped())
+		{
+			speedBoostTimer.Stop();
+		}
+		speedBoostTimer.WaitTime = SpeedBoostDuration;
+		speedBoostTimer.Start();
+	}
+
+	public void OnSpeedBoostTimerTimeout()
+	{
+		playerNode.IsBoosted = false;
 	}
 
 }
